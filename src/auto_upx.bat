6bf4e22b3c95c1deb835b24547c5ff7b452f57d7
steve02081504 <steve02081504@EDEN.fukwold>
2022-07-25 19:58:26 +0800

    little fixes
    Co-Authored-By: C.Ponapalt <83973+ponapalt@users.noreply.github.com>
    Co-Authored-By: Don <496156+nikolat@users.noreply.github.com>

--- src/auto_upx.bat
+++ src/auto_upx.bat
@@ -1,5 +1,5 @@
 cd Release
-"E:\tools\upx\upx.exe" --ultra-brute --no-align --best tama.exe
+upx --ultra-brute --no-align --best tama.exe
 copy tama.exe tama_NUPX.exe
-"E:\tools\upx\upx.exe" -d tama_NUPX.exe
+upx -d tama_NUPX.exe
 cd ..
